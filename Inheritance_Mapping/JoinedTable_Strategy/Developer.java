ea1925781054d658870ab0e786daea31dc858c05
sanketkumar100 <sanketkumar7645@gmail.com>
2025-10-01 15:51:59 +0530

    Child class

---
+++ Inheritance_Mapping/JoinedTable_Strategy/Developer.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Developer extends Employee
+{
+    private String programmingLanguage;
+    public Developer()   //used to instantiate object while fething from DB
+    {
+
+    }
+    public Developer(String programmingLanguage)
+    {
+        this.programmingLanguage=programmingLanguage;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+}
